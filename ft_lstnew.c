805c2ac2bab9d3d498ff82b5a0680587a20a7597
SnakePiton <barisvarmaz@hotmail.com>
2024-11-04 23:00:37 +0300

    dsad

---
+++ ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bv10 <bv10@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 21:29:56 by bv10              #+#    #+#             */
+/*   Updated: 2024/11/04 21:52:20 by bv10             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *) malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node ->content = content;
+	node ->next = NULL;
+	return (node);
+}
